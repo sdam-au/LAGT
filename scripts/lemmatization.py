8cfc937891b6f2c8bb6769d1f96a21d52984b705
kasev <vojtech.kase@gmail.com>
2020-12-04 10:51:08 +0100

    merged removed

--- scripts/lemmatization.py
+++ scripts/lemmatization.py
@@ -6,7 +6,6 @@ import spacy
 
 nlp = spacy.load("../models/spacy_grc_model_6")
 ag_lemma_lookup = nlp.vocab.lookups.get_table("lemma_lookup")
-ag_lemma_lookup_merged = nlp.vocab.lookups.get_table("lemma_lookup_merged")
 
 def grave_to_acute(string):
     GRAVE = "\u0300"
@@ -34,43 +33,49 @@ def list_of_possible_accentuations(morph):
         return []
 
 
-def lemmatizer_v0(token_text, tag, old_lemma=None):
-    if (old_lemma==None) or (token_text == old_lemma):
-        lemma = token_text # start with assigning the word as it stands
-        try:
-            lemma = ag_lemma_lookup[tag][token_text]
-        except: 
-            try:
-                lemma = ag_lemma_lookup[tag][grave_to_acute(token_text)]
-            except:
-                morph_vars = list_of_possible_accentuations(token_text)
-                for var in morph_vars:
-                    lemma = morph_vars[0]
-                    try:
-                        lemma = ag_lemma_lookup[tag][var] 
-                        break
-                    except: 
-                        pass
-        return lemma
-    else:
-        return old_lemma
-
 def grc_doc_lemmatizer(doc):
     for token in doc:
-        token.lemma_ = grc_lemmatizer(token.text, token.pos_, token.lemma_)
+        token.lemma_, token.pos_ = lemmatizer(token.text, token.pos_, token.lemma_)
     return doc   
 
 def apply_nlp(sentences_list):
     spacy_docs = []
     for sentence in sentences_list:
         doc = nlp(sentence)
-        doc = update_lemmata(doc)
         spacy_docs.append(doc)
     return spacy_docs
-    
-def grc_lemmatizer(token_text, tag, old_lemma=None):
+
+def check_char_validity(token_text, tag):
+    if re.search("\W", token_text):
+        if re.match("\W+$", token_text):
+            tag = "PUNCT"
+        else:
+            if "’" not in token_text:
+                token_text = re.sub("\W", "", token_text)
+    return token_text, tag
+
+# for new tags in specific order...
+
+keys = ['PRON', 'ADP', 'PUNCT', 'DET', 'CCONJ', 'INTJ', 'PART', 'ADJ', 'AUX', 'SCONJ', 'ADV', 'NOUN','PROPN', 'VERB','X', 'NUM']
+ 
+def check_other_tags(token_text, tag):
+    lemma = token_text
+    match = False
+    # for new tags in specific order...
+    for new_tag in keys:
+        try:
+            lemma = ag_lemma_lookup[new_tag][token_text]
+            tag = new_tag
+            match = True
+            break
+        except:
+            pass
+    return lemma, tag, match
+
+
+def lemmatizer_v1(token_text, tag, old_lemma=None):
     if (old_lemma==None) or (token_text == old_lemma):
-        lemma = token_text.lower() # start with assigning the word as it stands
+        lemma = token_text.lower() # start with assigning the word as it stands
         try:
             lemma = ag_lemma_lookup[tag][token_text]
         except:
@@ -94,6 +99,37 @@ def grc_lemmatizer(token_text, tag, old_lemma=None):
                                     break
                                 except:
                                     pass
-        return lemma
+        return lemma, tag
     else:
-        return old_lemma
+        return old_lemma, tag
+    
+def lemmatizer(token_text, tag, old_lemma=None):
+    if (old_lemma==None) or (token_text == old_lemma):
+        token_text, tag = check_char_validity(token_text, tag)
+        lemma = token_text.lower() # start with assigning the word as it stands
+        try:
+            lemma = ag_lemma_lookup[tag][token_text]
+        except:
+            try:
+                lemma = ag_lemma_lookup[tag][grave_to_acute(token_text)]
+            except:
+                if check_other_tags(token_text, tag)[2] == True:
+                    lemma, tag, match = check_other_tags(token_text, tag)
+                else:
+                        morph_vars = list_of_possible_accentuations(token_text)
+                        for var in morph_vars:
+                            try:
+                                try:
+                                    lemma = ag_lemma_lookup[tag][var] 
+                                    break
+                                except:
+                                    a, b, match = check_other_tags(var, tag)
+                                    if match ==True:
+                                        lemma, tag, match = check_other_tags(var, tag)
+                                        break
+                            except:
+                                pass
+
+        return lemma, tag
+    else:
+        return old_lemma, tag
